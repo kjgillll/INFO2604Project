a610385b79c718bf28ca1d0bac15b130789a2a3f
kjgillll <keronn.gill@my.uwi.edu>
2020-05-08 02:36:18 +0000

    ,

--- project/src/App.js
+++ project/src/App.js
@@ -8,25 +8,7 @@ import { Container, Header, Content, Footer, Sidebar } from 'rsuite';
 
 function App() {
   return (
-    <div className="App">
-        const headerStyles = {
-            padding: 18,
-            fontSize: 16,
-            height: 56,
-            background: '#34c3ff',
-            color: ' #fff',
-            whiteSpace: 'nowrap',
-            overflow: 'hidden'
-        };   
-
-        const iconStyles = {
-            width: 56,
-            height: 56,
-            lineHeight: '56px',
-            textAlign: 'center'
-        };
-
-
+    <div className="App" >
 
     </div>
   );
