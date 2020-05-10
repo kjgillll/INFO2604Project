525b85c5a21c68df8d51bdeb7c0677742c24bc97
kjgillll <keronn.gill@my.uwi.edu>
2020-05-10 06:43:11 +0000

    basic flask routes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -8,8 +8,31 @@ def index():
 
 @app.route("/veggies")
 def veggies():
-    return render_template("veggies.html") 
+    return render_template("/veggies.html") 
+
+@app.route("/bakery") 
+def bakery(): 
+    return render_template("/bakery.html") 
+
+
+@app.route("/personal") 
+def personal(): 
+    return render_template("/personal.html") 
 
     
+@app.route("/other") 
+def other(): 
+    return render_template("/other.html") 
+
+    
+@app.route("/frozen") 
+def frozen(): 
+    return render_template("/frozen.html") 
+
+    
+@app.route("/beverages") 
+def beverages(): 
+    return render_template("/beverages.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
